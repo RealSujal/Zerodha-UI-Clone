33291a36243ac9ee3e63fa8f1bf5aa65cc0dca0c
Sujal Mahajan <sujalmahajan24@gmail.com>
2024-02-11 13:30:30 +0530

    fixed some bugs

--- src/components/Home/SignupBtn.jsx
+++ src/components/Home/SignupBtn.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function SignupBtn() {
   return (
     <button className="bg-blue-600 hover:bg-black font-medium text-lg text-white px-8 py-2 rounded">
-      Sign up now
+      <NavLink to="signup">Sign up now</NavLink>
     </button>
   );
 }
