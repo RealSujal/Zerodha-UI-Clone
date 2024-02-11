27d40acf1a1dfb5d1544719ed072f8ad00abe25c
Sujal Mahajan <sujalmahajan24@gmail.com>
2024-02-11 13:17:43 +0530

    made some changes

--- src/components/Home/SignupBtn.jsx
+++ src/components/Home/SignupBtn.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function SignupBtn() {
+  const [isOpen, setOpen] = useState(false);
+  const click = () => {
+    setOpen(isOpen(false));
+  };
+
   return (
     <button className="bg-blue-600 hover:bg-black font-medium text-lg text-white px-8 py-2 rounded">
-      <NavLink to="signup">Sign up now</NavLink>
+      <NavLink onClick={click} to="signup">
+        Sign up now
+      </NavLink>
     </button>
   );
 }
